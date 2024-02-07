b06f03cc8ed3711c7565f5be3a6b2e3ae89a2b44
GasparScienza <gasparscienza@hotmail.com>
2024-02-06 22:13:37 -0300

    Ver certificados

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ function seleccionar(){
     document.getElementById("nav").classList="";
     menuVisible = false;
 }
+
+function mostrarImagen(src) {
+    document.getElementById('imagenAmpliadaSrc').src = src;
+    document.getElementById('imagenAmpliada').style.display = 'block';
+}
+
+function cerrarImagen() {
+    document.getElementById('imagenAmpliada').style.display = 'none';
+}
